5c8e8712f1d8e31210e4cd94d43f9fbfa53d2eb9
msklyar <michael.sklyar@cyren.com>
2017-09-03 21:19:49 +0300

    Added a bug

--- src/test/java/samle/webserver/math/TestMathOperations.java
+++ src/test/java/samle/webserver/math/TestMathOperations.java
@@ -9,7 +9,8 @@ public class TestMathOperations {
   @Test
   public void testFibonacci(){
     assertEquals("Fibonacci for 1 should be 1", 1, MathOperations.calculateFibonacci(1));
-    assertEquals("Fibonacci for 2 should be 1", 1, MathOperations.calculateFibonacci(2));
+    //This will fail
+    assertEquals("Fibonacci for 2 should be 1", 3, MathOperations.calculateFibonacci(2));
     assertEquals("Fibonacci for 10 should be 55", 55, MathOperations.calculateFibonacci(10));
   }
 }
